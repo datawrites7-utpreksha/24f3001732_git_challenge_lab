eb3ec9a271eb5f6d8d4261673867c0d318b22a95
Utpreksha <24f3001732@ds.study.iitm.ac.in>
2025-09-17 18:58:47 +0530

    add sum,difference,product files

---
+++ difference.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter second number,smaller than first number"))
+c=a-b
+print("Difference of two numbers is:",c)
---
+++ sum.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter second number"))
+c=a+b
+print("Sum of two numbers is:",c)
